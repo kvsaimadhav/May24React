d4dee4195ae6020861f6bb6e9db7c3b8cdfda347
Kaza Venkata Sai Madhav <kvsaimadhav@gmail.com>
2021-05-24 23:00:15 -0400

    Contact Phone number update with Space

--- src/Components/Contact/Contact.js
+++ src/Components/Contact/Contact.js
@@ -12,7 +12,7 @@ function Contact() {
           <h1 className="display-3">My Contact Details</h1>
           <p className="lead">Here are my contact details </p>
           <p className="lead">Email : kvsaimadhav@gmail.com </p>
-          <p className="lead">Phone : 7345897242 </p>
+          <p className="lead">Phone : 734 589 7242 </p>
           <p className="lead">Typical Wake up / Sleep Hours : Other than bootcamp. I will available morning 10AM to 7PM EST via slack or whatsapp  </p>
         </Container>
       </Jumbotron>
